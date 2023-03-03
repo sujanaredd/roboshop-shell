c33329e671906f2bc24788cb25e0fd6a5e31067d
sujanredd <sujanareddy1024@gmail.com>
2023-03-03 22:19:02 +0530

    Roboshop Components

---
+++ mongodb.sh
@@ -0,0 +1,6 @@
+cp conflics/mongo.db.repo /etc/yum.repos.4/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+# Update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
